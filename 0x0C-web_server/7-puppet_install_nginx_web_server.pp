6d77974922ff32b15f92aa5810898d59dfe96518
koko-cs23 <dennisthebusinessguru@gmail.com>
2023-04-26 22:59:57 +0100

    fixed issues

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,67 @@
-# Installs a Nginx server
-
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/koko-cs23 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
 }
+
+# Set Nginx to listen on port 80
+file_line { 'nginx_listen':
+  path    => '/etc/nginx/sites-available/default',
+  line    => '    listen 80 default_server;',
+  match   => '^    listen\s*80;',
+  replace => true,
+}
+
+# Configure error page
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# Configure Nginx to serve the redirect_me file
+file { '/etc/nginx/sites-available/default':
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    error_page 404 /404.html;
+    location = /404.html{
+       internal;
+    }
+
+    location /redirect_me {
+        return 301 http://www.redirectedpage.com/;
+    }
+}
+  ",
+  notify  => Service['nginx'],
+}
+
+# Test Nginx configuration for syntax errors
+exec { 'nginx_test_config':
+  command     => '/usr/sbin/nginx',
+  logoutput   => true,
+  refreshonly => true,
+}
+
+# Restart Nginx service if the configuration is valid
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  subscribe  => File['/etc/nginx/sites-available/default'],
+  require    => Exec['nginx_test_config'],
+}
+# Create index file with "Hello World!" message
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!\n',
+}
+
+# Create redirect_me file with redirect information
+file { '/var/www/html/redirect_me':
+  content => "HTTP/1.1 301 Moved Permanently\r\nLocation: http://www.redirectedpage.com/\r\n\r\n",
+}
+
